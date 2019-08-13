076b98af30005a5d0f628a2bec5b6e6216d54d85
luiscal4a <luiscal4a@gmail.com>
2019-08-13 23:55:15 +0200

    UI redisign and photo functionality

--- AR_Builder/app/src/main/java/com/example/arbuilder/MainActivity.java
+++ AR_Builder/app/src/main/java/com/example/arbuilder/MainActivity.java
@@ -2,37 +2,55 @@ package com.example.arbuilder;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.PixelCopy;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.core.Pose;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.HitTestResult;
 import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.collision.CollisionShape;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.MaterialFactory;
@@ -46,9 +64,15 @@ import com.skydoves.colorpickerview.ColorEnvelope;
 import com.skydoves.colorpickerview.ColorPickerDialog;
 import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 
@@ -62,9 +86,10 @@ public class MainActivity extends AppCompatActivity {
     private AnchorNode myanchornode;
 
     private TextView text;
-    private SeekBar sk_height_control;
-    private Button btn_clear, btn_new;
+    private VerticalSeekBar sk_height_control;
+    private ImageButton btn_clear, btn_new;
     private Button btn_color;
+    private ProgressBar loading;
 
     private int color = android.graphics.Color.RED;
     float data1 = 0.25f, data2 = 0.25f, data3 = 0.25f;
@@ -84,6 +109,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TransformableNode transformableNode;
 
+    private ArSceneView sceneView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,14 +128,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        if (ContextCompat.checkSelfPermission( MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions( MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        if (ContextCompat.checkSelfPermission( MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions( MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+        }
+
+
+
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
         text = (TextView) findViewById(R.id.text);
 
-        sk_height_control = (SeekBar) findViewById(R.id.sk_height_control);
-        btn_new = (Button) findViewById(R.id.btn_new);
-        btn_clear = (Button) findViewById(R.id.btn_clear);
+        sk_height_control = (VerticalSeekBar) findViewById(R.id.sk_height_control);
+        btn_new = (ImageButton) findViewById(R.id.btn_new);
+        btn_clear = (ImageButton) findViewById(R.id.btn_clear);
+        ImageButton btn_raise = (ImageButton) findViewById(R.id.btn_raise);
+        loading = (ProgressBar) findViewById(R.id.loading);
+
+
+        ImageButton mybtn = (ImageButton) findViewById(R.id.take_photo);
 
         sk_height_control.setEnabled(false);
+        sk_height_control.setMax(500);
+
+        btn_raise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(sk_height_control.isEnabled()) {
+                    sk_height_control.raiseScaled();
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Tap plane to place object",
+                            Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loading.setVisibility(View.VISIBLE);
+                takePhoto(sceneView);
+            }
+        });
 
 
         btn_new.setOnClickListener(new View.OnClickListener() {
@@ -160,6 +222,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         makeMaterial(0, color);
+
+        sceneView = arFragment.getArSceneView();
 
 
         arFragment.setOnTapArPlaneListener(
@@ -394,6 +458,89 @@ public class MainActivity extends AppCompatActivity {
                             ballRenderable = rd_externalModels[index].makeCopy();
                             ballRenderable.setMaterial(material);
                         }
+                    }
+                });
+    }
+
+    private String generateFilename() {
+        String date =
+                new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+        return Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + File.separator + "AR_Builder/" + date + "_screenshot.jpg";
+    }
+
+    private void saveBitmapToDisk(Bitmap bitmap, String filename) throws IOException {
+
+        File out = new File(filename);
+        if (!out.getParentFile().exists()) {
+            out.getParentFile().mkdirs();
+        }
+        try (FileOutputStream outputStream = new FileOutputStream(filename);
+             ByteArrayOutputStream outputData = new ByteArrayOutputStream()) {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputData);
+            outputData.writeTo(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException ex) {
+            throw new IOException("Failed to save bitmap to disk", ex);
+        }
+    }
+
+    private void takePhoto(ArSceneView sceneView) {
+        final String filename = generateFilename();
+        /*ArSceneView view = fragment.getArSceneView();*/
+        // Create a bitmap the size of the scene view.
+        final Bitmap bitmap = Bitmap.createBitmap(sceneView.getWidth(), sceneView.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        // Create a handler thread to offload the processing of the image.
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+        handlerThread.start();
+        // Make the request to copy.
+        PixelCopy.request(sceneView, bitmap, (copyResult) -> {
+            if (copyResult == PixelCopy.SUCCESS) {
+                try {
+                    saveBitmapToDisk(bitmap, filename);
+                } catch (IOException e) {
+                    Toast toast = Toast.makeText(MainActivity.this, e.toString(),
+                            Toast.LENGTH_LONG);
+                    toast.show();
+                    return;
+                }
+
+                File myfile = new File(filename);
+                Uri fileUri = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ?
+                        FileProvider.getUriForFile(this,getPackageName() + ".provider", myfile) : Uri.fromFile(myfile);
+
+                notifyGalleryChange(myfile);
+                loading.setVisibility(View.INVISIBLE);
+                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                        "Photo saved", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Open in Photos", v -> {
+                    openImage(fileUri);
+                });
+                snackbar.show();
+            } else {
+                Log.d("DrawAR", "Failed to copyPixels: " + copyResult);
+                Toast toast = Toast.makeText(MainActivity.this,
+                        "Failed to copyPixels: " + copyResult, Toast.LENGTH_LONG);
+                toast.show();
+            }
+            handlerThread.quitSafely();
+        }, new Handler(handlerThread.getLooper()));
+    }
+
+    private void openImage(Uri fileUri){
+        final Intent intent = new Intent(Intent.ACTION_VIEW)//
+                .setDataAndType(fileUri, "image/*").addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
+    }
+
+    private void notifyGalleryChange(File file){
+        MediaScannerConnection.scanFile(this, new String[] {
+                        file.getAbsolutePath()},
+                null, new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
                     }
                 });
     }
